3953f35797fb57576804cab15b698073e9bfa3a1
Vladimir <vova2066@yandex.ru>
2023-12-24 23:49:46 +0300

    ++

--- src/blocks/selection/selection.js
+++ src/blocks/selection/selection.js
@@ -4,6 +4,16 @@ if (selection) {
   const tabs = selection.querySelectorAll("[data-tab]");
   const types = selection.querySelectorAll("[data-type]");
 
+  const largeTabs = selection.querySelectorAll(".selection__tabs-large li");
+  largeTabs[0].classList.add('js-tab-active');
+  largeTabs.forEach(largeTab => {
+    largeTab.addEventListener("click", () => {
+      largeTabs.forEach(tab => tab.classList.remove('js-tab-active'));
+      largeTab.classList.add('js-tab-active');
+    });
+  });
+
+
   types.forEach(type => type.classList.add('js-hidden'));
   types[0].classList.remove('js-hidden');
   tabs[0].classList.add('js-tab-active');
